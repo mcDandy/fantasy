55d3662b11fa2d472f97ff55137e0a4cf9ae3dcf
mcDandy <danda.martinek@gmail.com>
2016-04-21 11:21:28 +0200

    recipes rebalanced

--- fantasyCode/src/main/java/com/mc_Dandy/winx/items/ModItems3.java
+++ fantasyCode/src/main/java/com/mc_Dandy/winx/items/ModItems3.java
@@ -2,13 +2,12 @@ package com.mc_Dandy.winx.items;
 
 
 
+import com.mc_Dandy.fantasyCore.items.ModItems;
+
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-
-import com.mc_Dandy.fantasyCore.items.BlueDust;
-import com.mc_Dandy.fantasyCore.items.ModItems;
 
 
 public final class ModItems3 {
@@ -18,8 +17,8 @@ public final class ModItems3 {
 	public static void createItems() {
 	       GameRegistry.registerItem(fp = new FiaryPowder("fp"), "fiary dust");
 
-		GameRegistry.addShapelessRecipe(new ItemStack(ModItems3.fp),
-				new Object[] { ModItems.Magic_Powder, Items.glowstone_dust, new ItemStack(Items.dye, 1, 4) });
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems3.fp,10),
+				new Object[] { ModItems.Magic_Powder, Items.glowstone_dust, Items.glowstone_dust, new ItemStack(Items.dye, 1, 4) });
 
 	}
 }
