a4af71569ed2514e263c1edc33cdb2ca9cca3230
mcDandy <danda.martinek@gmail.com>
2016-04-21 10:58:53 +0200

    wings removed, fiary dust added
    I removed wings due to unrepairable errors and added fiary dust for
    future uses (full stack is 255)

---
+++ fantasyCode/src/main/java/com/mc_Dandy/winx/items/FiaryPowder.java
@@ -0,0 +1,11 @@
+package com.mc_Dandy.winx.items;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class FiaryPowder extends Item {
+	public FiaryPowder(String string){
+		this.setCreativeTab(CreativeTabs.tabMaterials);
+		this.setMaxStackSize(255);
+	}
+}
--- fantasyCode/src/main/java/com/mc_Dandy/winx/items/ModItems3.java
+++ fantasyCode/src/main/java/com/mc_Dandy/winx/items/ModItems3.java
@@ -6,6 +6,8 @@ import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+
+import com.mc_Dandy.fantasyCore.items.BlueDust;
 import com.mc_Dandy.fantasyCore.items.ModItems;
 
 
@@ -14,7 +16,7 @@ public final class ModItems3 {
 	public static Item fp;
 
 	public static void createItems() {
-	       GameRegistry.registerItem((new ItemElytra()).setUnlocalizedName("elytra"), "elytra");
+	       GameRegistry.registerItem(fp = new FiaryPowder("fp"), "fiary dust");
 
 		GameRegistry.addShapelessRecipe(new ItemStack(ModItems3.fp),
 				new Object[] { ModItems.Magic_Powder, Items.glowstone_dust, new ItemStack(Items.dye, 1, 4) });
