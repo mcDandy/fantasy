4ce9394f926c7c17a14de8706360ce02c61c71b9
mcDandy <danda.martinek@gmail.com>
2016-05-27 09:57:50 +0200

    flying is repaired
    I have issues with fluid

--- fantasyCode/src/main/java/cz/mc_Dandy/fantasyCore/abilities/flying.java
+++ fantasyCode/src/main/java/cz/mc_Dandy/fantasyCore/abilities/flying.java
@@ -1,17 +1,18 @@
 package cz.mc_Dandy.fantasyCore.abilities;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 
 public class flying {
-/*	@SubscribeEvent
+	@SubscribeEvent
 	void plaierJoin(EntityJoinWorldEvent e){
-		CanFly(null);
+		CanFly(null, null);
 	}
-	public void CanFly(NBTTagCompound nbt){
-		if (nbt.getInteger("postava")==2) {nbt.setBoolean("mayfly", true);} else {nbt.setBoolean("mayfly", false);};
-	};*/
+	public void CanFly(NBTTagCompound nbt, EntityPlayer player){
+		if (nbt.getInteger("postava")==2) {player.capabilities.allowFlying = true;} else {player.capabilities.allowFlying = false;player.capabilities.isFlying = false;};
+	};
 	
 }
---
+++ fantasyCode/src/main/java/cz/mc_Dandy/winx/fluid/morfix.java
@@ -0,0 +1,18 @@
+package cz.mc_Dandy.winx.fluid;
+
+import cz.mc_Dandy.winx.main.Winx;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
+
+class morfix{
+	public static Fluid morfix;
+	public void registerFluids()
+	{
+		FluidRegistry.registerFluid(morfix
+				.setViscosity(12000) //voda 1 000, láva 6 000 
+				.setUnlocalizedName(Winx.MODID+":morfix")
+				.setLuminosity(3)
+				);
+	}
+	
+}
