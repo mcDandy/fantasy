b8b73c3bdf9cf60f6fa645098ddd197c2c0dd297
mcDandy <danda.martinek@gmail.com>
2016-06-07 18:57:37 +0200

    zbitečné řádky

--- Marmid mod/src/main/java/cz/mc_Dandy/marmid/blocks/ModBlocks.java
+++ Marmid mod/src/main/java/cz/mc_Dandy/marmid/blocks/ModBlocks.java
@@ -3,7 +3,7 @@ package cz.mc_Dandy.marmid.blocks;
 import net.minecraft.block.Block;
 
 public final class ModBlocks {
-	public static Block blueGlowstone;
+
 
 	public static void createBlocks() {
 	}
